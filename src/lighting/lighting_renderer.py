835a7e3eae53e5f00727926c5e3624ad9da11937
moiseshb7777 <moiseshb777@gmail.com>
2026-09-16 16:12:38 +0100

    Vamos começar a dividir responsabilidades

---
+++ src/lighting/lighting_renderer.py
@@ -0,0 +1,7 @@
+class LightingRenderer:
+    """ def __init__(self, screen):
+        self.screen = screen
+        self.darkness = pygame.Surface(screen.get_size(), pygame.SRCALPHA)
+        self.light_gradient = pygame.Surface(screen.get_size(), pygame.SRCALPHA)
+        self.light_mask = pygame.Surface(screen.get_size(), pygame.SRCALPHA) """
+    pass
